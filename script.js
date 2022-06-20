d3563f49ecea4d3314e3efc1fd4d5ec7cdf2073d
subramani <106590803+subu-v@users.noreply.github.com>
2022-06-20 23:36:01 +0530

    Finishes version, animations didn't do.

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-let buttonOpen = document.querySelectorAll(".accordian__question");
+let accordianButton = document.querySelectorAll(".accordian__question");
 
-for (let i = 0; i < buttonOpen.length; i++) {
-  buttonOpen[i].addEventListener("click", function () {
-    buttonOpen[i].classList.toggle("open");
-    buttonOpen[i].nextElementSibling.classList.toggle("open");
+accordianButton.forEach((accordian) => {
+  accordian.addEventListener("click", (event) => {
+    accordian.classList.toggle("open");
+    accordian.nextElementSibling.classList.toggle("open");
   });
-}
+});
+
+// My old way of adding eventListener to elements.
+
+// for (let i = 0; i < buttonOpen.length; i++) {
+//   buttonOpen[i].addEventListener("click", function () {
+//     buttonOpen[i].classList.toggle("open");
+//     buttonOpen[i].nextElementSibling.classList.toggle("open");
+//   });
+// }
