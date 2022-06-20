b7d9d8798117c01dbb457078cf37cf1ff7c46cf8
subramani <106590803+subu-v@users.noreply.github.com>
2022-06-20 22:38:15 +0530

    Added the functionality in js, just animations are left

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("is it working?");
+let buttonOpen = document.querySelectorAll(".accordian__question");
+
+for (let i = 0; i < buttonOpen.length; i++) {
+  buttonOpen[i].addEventListener("click", function () {
+    buttonOpen[i].classList.toggle("open");
+    buttonOpen[i].nextElementSibling.classList.toggle("open");
+  });
+}
